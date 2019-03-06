f0a9136cad7aa68751a3e981e6674042678e41a6
Gavin Watson <gavset99@gmail.com>
2019-03-05 20:01:03 -0500

    Added basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home.js';
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
+    const App = () => (
+      <Switch>
+        <Route exact path='/' component={Home}/>
+      </Switch>
+    );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Route>
+        <App/>
+      </Route>
     );
   }
 }
